d6891451ee80c4764b8d374481a0650df53869b5
PedroLuis-Ape <freeluizy@gmail.com>
2026-06-19 15:30:57 -0300

    feat: resolve list id from pathname

--- src/lib/ListDirectionGate.tsx
+++ src/lib/ListDirectionGate.tsx
@@ -1,11 +1,13 @@
 import { useEffect, useState, type ReactNode } from "react";
-import { useLocation, useNavigate, useParams } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { loadListPrimarySide } from "./loadListPrimarySide";
 import { loadPublicSide } from "./loadPublicSide";
 import { primarySideToDirection } from "./primarySideDirection";
+import { listIdFromPath } from "./listRoute";
 
 export function ListDirectionGate({ children }: { children: ReactNode }) {
   const location = useLocation();
-  void location;
+  const listId = listIdFromPath(location.pathname);
+  void listId;
   return <>{children}</>;
 }
