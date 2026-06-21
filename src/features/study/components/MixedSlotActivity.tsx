f67ba971584613233d6f0fca4aa09106b4e0fbaf
PedroLuis-Ape <freeluizy@gmail.com>
2026-06-20 21:12:13 -0300

    fix(study): preserve card tools in mixed replacement activities

--- src/features/study/components/MixedSlotActivity.tsx
+++ src/features/study/components/MixedSlotActivity.tsx
@@ -1,3 +1,4 @@
+import type { MergedHint } from "@/features/study/lib/glossaryMerge";
 import { WriteStudyView } from "./WriteStudyView";
 import { PronunciationStudyView } from "./PronunciationStudyView";
 
@@ -5,12 +6,22 @@ interface MixedSlotActivityProps {
   mode: "write" | "pronunciation";
   front: string;
   back: string;
+  hint?: string | null;
   direction: string;
   flashcardId?: string;
+  wordHintsA?: unknown;
+  mergedHintsA?: MergedHint[];
+  mergedHintsB?: MergedHint[];
   langA?: string;
   langB?: string;
   labelA?: string;
   labelB?: string;
+  isFavorite?: boolean;
+  isRedListed?: boolean;
+  onToggleFavorite?: () => void;
+  onToggleRedList?: () => void;
+  isSpecial?: boolean;
+  onToggleSpecial?: () => void;
   onCorrect: () => void;
   onIncorrect: () => void;
 }
@@ -21,10 +32,19 @@ export function MixedSlotActivity(props: MixedSlotActivityProps) {
       <PronunciationStudyView
         front={props.front}
         back={props.back}
+        wordHintsA={props.wordHintsA}
+        mergedHintsA={props.mergedHintsA}
+        mergedHintsB={props.mergedHintsB}
         langA={props.langA}
         langB={props.langB}
         labelA={props.labelA}
         labelB={props.labelB}
+        isFavorite={props.isFavorite}
+        isRedListed={props.isRedListed}
+        onToggleFavorite={props.onToggleFavorite}
+        onToggleRedList={props.onToggleRedList}
+        isSpecial={props.isSpecial}
+        onToggleSpecial={props.onToggleSpecial}
         onNext={props.onCorrect}
       />
     );
@@ -34,10 +54,20 @@ export function MixedSlotActivity(props: MixedSlotActivityProps) {
     <WriteStudyView
       front={props.front}
       back={props.back}
+      hint={props.hint}
       flashcardId={props.flashcardId}
+      wordHintsA={props.wordHintsA}
+      mergedHintsA={props.mergedHintsA}
+      mergedHintsB={props.mergedHintsB}
       direction={props.direction}
       langA={props.langA}
       langB={props.langB}
+      isFavorite={props.isFavorite}
+      isRedListed={props.isRedListed}
+      onToggleFavorite={props.onToggleFavorite}
+      onToggleRedList={props.onToggleRedList}
+      isSpecial={props.isSpecial}
+      onToggleSpecial={props.onToggleSpecial}
       onCorrect={props.onCorrect}
       onIncorrect={props.onIncorrect}
       onSkip={props.onIncorrect}
